8597213311f1e8762954351096de3bf5d543bb5f
Mahesh-Chaulagain <maheshchaulagain8@gmail.com>
2023-10-02 21:00:25 +0545

    feched data from the weather API

--- weather.py
+++ weather.py
@@ -1,3 +1,23 @@
-import requests 
+from dotenv import load_dotenv
+import os
+import requests
 
-API_KEY = "Your API key"
+# Load environment variables from .env file
+load_dotenv()
+
+# Retrieve the value of the environment variable
+API_KEY = os.getenv("API_KEY")  #get API_KEY from .env file
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather" #url for API
+
+city = input("Enter a city name: ")
+request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"    #append api key and city to base url
+response = requests.get(request_url)    #use get request to retrieve information
+
+if response.status_code == 200:
+    data = response.json()
+    weather = data['weather'][0]['description']   #access weather key from the json file
+    temperature = round(data["main"]["temp"] - 273.15,2) #access temp from the json
+    print("weather:",weather)
+    print("temperature",temperature,"celsius")
+else:
+    print("an error occured")
